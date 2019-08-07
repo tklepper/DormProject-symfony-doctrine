3e060174500d3cf9a1cd02c7c2db225e1aae2d78
Tony Klepper <tony@klepper.us>
2019-08-07 05:10:30 -0400

    Initial Commit

---
+++ src/Entity/Unit.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UnitRepository")
+ */
+class Unit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $building_number;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $floor_number;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $room_number;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBuildingNumber(): ?int
+    {
+        return $this->building_number;
+    }
+
+    public function setBuildingNumber(int $building_number): self
+    {
+        $this->building_number = $building_number;
+
+        return $this;
+    }
+
+    public function getFloorNumber(): ?int
+    {
+        return $this->floor_number;
+    }
+
+    public function setFloorNumber(int $floor_number): self
+    {
+        $this->floor_number = $floor_number;
+
+        return $this;
+    }
+
+    public function getRoomNumber(): ?int
+    {
+        return $this->room_number;
+    }
+
+    public function setRoomNumber(int $room_number): self
+    {
+        $this->room_number = $room_number;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dob;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $student_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $unit_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getZip(): ?int
+    {
+        return $this->zip;
+    }
+
+    public function setZip(int $zip): self
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getDob(): ?\DateTimeInterface
+    {
+        return $this->dob;
+    }
+
+    public function setDob(\DateTimeInterface $dob): self
+    {
+        $this->dob = $dob;
+
+        return $this;
+    }
+
+    public function getStudentId(): ?int
+    {
+        return $this->student_id;
+    }
+
+    public function setStudentId(int $student_id): self
+    {
+        $this->student_id = $student_id;
+
+        return $this;
+    }
+
+    public function getUnitId(): ?int
+    {
+        return $this->unit_id;
+    }
+
+    public function setUnitId(int $unit_id): self
+    {
+        $this->unit_id = $unit_id;
+
+        return $this;
+    }
+}
